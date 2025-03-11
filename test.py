0ca6021651a1aabf90a97cd7d1e1eb6b808329fc
Joep van Genuchten <joep.van.genuchten@gmail.com>
2025-03-11 14:12:35 +0100

    fixed some issues

--- test.py
+++
@@ -1,4 +0,0 @@
-from SimpleMDDataCatalog.spreadsheet_to_ld import spreadsheet_to_ld_catalog
-
-
-spreadsheet_to_ld_catalog(uri="https://www.test.com#", output_graph='test.ttl', input_sheet='/test/catalog.xlsx')
